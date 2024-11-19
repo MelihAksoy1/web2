1ed4c7f785e0fa772ba9c9bae98eac9f808a39a8
Melih Aksoy <melih.aksoy@student.vinci.be>
2024-11-19 10:58:15 +0100

    new: ex2.10

--- tutorials/medium-state/src/types.ts
+++ tutorials/medium-state/src/types.ts
@@ -1,8 +1,11 @@
 interface Pizza {
-    id: number;
-    title: string;
-    content: string;
-  }
-  
-  export type { Pizza };
+  id: number;
+  title: string;
+  content: string;
+}
+
+type NewPizza = Omit<Pizza, "id">;
+
+export type { Pizza, NewPizza };
+
   
